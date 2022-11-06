a06896d007e8c27b59bd3677ef464f028a94d65f
Kingomac <kingomacyt@gmail.com>
2022-11-06 15:11:38 +0100

    #4 en esencia no funcionaba, funciona y comentado

--- js/gestion-permisos/gestion-permisos.js
+++ js/gestion-permisos/gestion-permisos.js
@@ -1,3 +1,5 @@
+const ID_ROL_ADMIN = 0;
+
 /**
  * 
  * @param {{
@@ -18,13 +20,15 @@
         }
       }[] | undefined} datos 
  */
-async function actualizarTabla(datos) {
+async function actualizarTablaPermisos(datos) {
   // Reset del tbody
   const tbody = document.getElementById("table_body");
   tbody.textContent = "";
   // Reset del thead
-  for (const i of document.getElementById("table_title").children) {
-    if (!i.classList.contains("txt")) i.remove();
+  const trTitulo = document.getElementById("table_title");
+  while (trTitulo.children.length > 2) {
+    if (!trTitulo.children[2].classList.contains("txt"))
+      trTitulo.children[2].remove();
   }
   if (datos == null) {
     try {
@@ -38,54 +42,53 @@ async function actualizarTabla(datos) {
     }
   }
   const roles = await getRoles();
-  const trTitulo = document.getElementById("table_title");
   for (const i of roles) {
     const th = document.createElement("th");
     th.append(i.nombre_rol);
     trTitulo.append(th);
   }
-  for (let i = 0; i < datos.length; i++) {
-    let tr = crearTR(
+
+  let i = 0;
+  while (i < datos.length) {
+    // bucle recorrer datos (se recorre esto porque no sabes el número de filas, ya que no se hace fetch de pares funcionalidad-accion)
+    const tr = crearTR(
+      // se crea una fila
       datos[i].id_funcionalidad.nombre_funcionalidad,
       datos[i].id_accion.nombre_accion
     );
-    const estado = {
-      id_funcionalidad: datos[i].id_funcionalidad.id_funcionalidad,
-      id_accion: datos[i].id_accion.id_accion,
-    };
     for (let j = 0; j < roles.length; j++) {
-      // Como los datos del back están ordenados por Rol se puede recorrer según los roles hasta que
-      // deja de haber datos de la misma funcionalidad
-      try {
-        if (
-          i < datos.length &&
-          estado.id_funcionalidad ==
-            datos[i].id_funcionalidad.id_funcionalidad &&
-          estado.id_accion == datos[i].id_accion.id_accion
-        ) {
-          const [btnTick, btnDelete] = crearBtnAddDelete({
-            tienePermiso: true,
-            id_accion: datos[i].id_accion.id_accion,
-            id_funcionalidad: datos[i].id_funcionalidad.id_funcionalidad,
-            id_rol: datos[i].id_rol.id_rol,
-          });
-          tr.append(crearTd(btnTick, btnDelete));
-          i++;
-        } else {
-          const [btnTick, btnDelete] = crearBtnAddDelete({
-            tienePermiso: false,
-            id_accion: estado.id_accion,
-            id_funcionalidad: estado.id_funcionalidad,
-            id_rol: roles[j].id_rol,
-          });
-          tr.append(crearTd(btnTick, btnDelete));
-        }
-      } catch (err) {
-        console.error(err);
-        console.log("i:", i, "datos:", datos[i]);
+      // se iteran los roles para añadir cada uno a la fila
+      if (i < datos.length && roles[j].id_rol == datos[i].id_rol.id_rol) {
+        // si el dato seleccionado tiene el rol de la columna a crear
+        // se añaden botones con funcionalidad de quitar el permiso
+        // y se pasa al siguiente dato
+        tr.append(
+          crearTd(
+            ...crearBtnAddDelete({
+              tienePermiso: true,
+              id_accion: datos[i].id_accion.id_accion,
+              id_funcionalidad: datos[i].id_funcionalidad.id_funcionalidad,
+              id_rol: roles[j].id_rol,
+            })
+          )
+        );
+        i++; // i al final del bucle se queda en el primer dato de la siguiente fila, porque solo se incrementa
+        // cuando hay un rol de esa fila que tiene el rol
+      } else {
+        // si lo anterior no se cumple, significa que el rol no tiene permiso en ese funcionalidad-accion
+        // y se crean los botones que permiten darle el permiso
+        tr.append(
+          crearTd(
+            ...crearBtnAddDelete({
+              tienePermiso: false,
+              id_accion: datos[i - 1].id_accion.id_accion,
+              id_funcionalidad: datos[i - 1].id_funcionalidad.id_funcionalidad,
+              id_rol: roles[j].id_rol,
+            })
+          )
+        );
       }
     }
-    i--;
     tbody.append(tr);
   }
 }
@@ -128,7 +131,8 @@ function crearBtnAddDelete({
 
   if (tienePermiso) {
     btnTick.style.filter = "grayscale(100%)";
-    btnDelete.onclick = () => deleteRolAccionFuncionalidad();
+    btnDelete.onclick = () =>
+      deleteRolAccionFuncionalidad({ id_accion, id_funcionalidad, id_rol });
   } else {
     btnDelete.style.filter = "grayscale(100%)";
     btnTick.onclick = () =>
@@ -138,7 +142,22 @@ function crearBtnAddDelete({
   return [btnTick, btnDelete];
 }
 
-function deleteRolAccionFuncionalidad() {}
+async function deleteRolAccionFuncionalidad({
+  id_funcionalidad,
+  id_accion,
+  id_rol,
+}) {
+  try {
+    await peticionBackDeleteRolAccionFuncionalidad({
+      id_accion,
+      id_funcionalidad,
+      id_rol,
+    });
+    await actualizarTablaPermisos();
+  } catch (err) {
+    mensajeError({ idInput: "id_caja_error", codigo: err });
+  }
+}
 
 async function addRolAccionFuncionalidad({
   id_funcionalidad,
@@ -146,12 +165,12 @@ async function addRolAccionFuncionalidad({
   id_rol,
 }) {
   try {
-    const peticion = await peticionBackAddRolAccionFuncionalidad({
+    await peticionBackAddRolAccionFuncionalidad({
       id_accion,
       id_funcionalidad,
       id_rol,
     });
-    console.log("peticion exitosa:", peticion);
+    await actualizarTablaPermisos();
   } catch (err) {
     mensajeError({ idInput: "id_caja_error", codigo: err });
   }
@@ -173,6 +192,41 @@ function peticionBackAddRolAccionFuncionalidad({
   datos.append("id_rol", id_rol);
   datos.append("controlador", "rolaccionfuncionalidad");
   datos.append("action", "ADD");
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      method: "POST",
+      url: URL_BACK,
+      data: datos,
+      processData: false,
+      contentType: false,
+    })
+      .done((res) => {
+        if (res.ok != true || res.code != "SQL_OK") {
+          reject(res);
+        } else resolve(res.resource);
+      })
+      .fail((res) => {
+        reject(res);
+      });
+  });
+}
+
+/**
+ *
+ * @param {{id_funcionalidad: number, id_accion: number, id_rol: number}} params
+ * @returns
+ */
+function peticionBackDeleteRolAccionFuncionalidad({
+  id_funcionalidad,
+  id_accion,
+  id_rol,
+}) {
+  const datos = new FormData();
+  datos.append("id_funcionalidad", id_funcionalidad);
+  datos.append("id_accion", id_accion);
+  datos.append("id_rol", id_rol);
+  datos.append("controlador", "rolaccionfuncionalidad");
+  datos.append("action", "DELETE");
   return new Promise((resolve, reject) => {
     $.ajax({
       method: "POST",
